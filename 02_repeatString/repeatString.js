8500976d06e3a7ba7414414c6b89172fad236026
Anthony Mack <mackattack713@gmail.com>
2022-12-08 14:16:22 -0600

    Completed exercise number 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+// const repeatString = function() {
 
-};
+// };
+
+function repeatString(input,num) {
+    let answer = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num >= 0) {
+        for(let i=0; i < num; i++) {
+            answer += input;
+        }
+    }
+
+    return answer;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
