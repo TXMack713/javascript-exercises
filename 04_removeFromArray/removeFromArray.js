e563db826ef87c6271ee1f16403da790e55196e8
Anthony Mack <mackattack713@gmail.com>
2022-12-11 12:16:13 -0600

    Completed this exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+// const removeFromArray = function() {
 
-};
+// };
+function removeFromArray(arr, ...item) {
+    let newArr = [];
+    let eliminateArr = [];
+    if (item.length == 1) {
+        for(let j = 0; j < arr.length; j++) {
+            if (arr[j] === item[0]) {
+                continue;
+            } else {
+                newArr.push(arr[j]);
+            }
+        }
+    } else {
+        // eliminateArr = item.split('');
+        for(let i = 0; i < arr.length; i++) {
+            if(item.includes(arr[i])) {
+                continue;
+            } else {
+                newArr.push(arr[i]);
+            }
+        }
+    }
+    
+    return newArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
