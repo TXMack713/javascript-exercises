f1ba635666d79273aa6a28ac96a6248ef5bc1fae
Anthony Mack <mackattack713@gmail.com>
2022-12-11 09:55:02 -0600

    Completed the Reverse String exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+// const reverseString = function() {
 
-};
-
+// };
+function reverseString(input) {
+    let inputArray = input.split('');
+    let answer = inputArray.reverse().join('');
+    return answer;
+}
 // Do not edit below this line
 module.exports = reverseString;
