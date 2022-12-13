024dc4ab266d269397be230f52bb267f3ff2cf8c
Anthony Mack <mackattack713@gmail.com>
2022-12-13 14:39:28 -0600

    Completed the temperature conversion task.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let conversion = ((tempF - 32) * 5) / 9;
+  let rounded = Math.round(conversion * 10) / 10;
+  return (rounded);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let conversion = (tempC * 1.8) + 32;
+  let rounded = Math.round(conversion * 10) / 10;
+  return (rounded);
 };
 
 // Do not edit below this line
