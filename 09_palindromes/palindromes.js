16b7e1144baf47a0606fd81245d5499d9c2094e8
Anthony Mack <mackattack713@gmail.com>
2022-12-16 16:15:21 -0600

    Completed the palidromes exercises.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (entry) {
+    // let original = Array.from(entry);
+    let entryRegex = /[a-z]/gi;
+    let original = entry.match(entryRegex).join('');
+    console.log("Original");
+    console.log(original);
+    let startSample = Array.from(original);
+    startSample.reverse();
+    let final = startSample.join('');
+    console.log("Final");
+    console.log(final);
+    return original.toLowerCase() == final.toLowerCase();
 };
 
 // Do not edit below this line
