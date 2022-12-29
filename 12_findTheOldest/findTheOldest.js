37957f5eafde11dfd7649ca443aad473c9666559
Anthony Mack <mackattack713@gmail.com>
2022-12-28 21:11:21 -0600

    Began working on the find the oldest Oding Project exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+    let ages = [];
+    for(let i = 0; i < arrayOfPeople.length; i++) {
+        let yearOfDeath = arrayOfPeople[i].yearOfDeath;
+        let yearOfBirth = arrayOfPeople[i].yearOfBirth;
+        let age = yearOfDeath - yearOfBirth;
+        console.log(age);
+        if (yearOfDeath == null) {
+            const date = new Date();
+            let year = date.getFullYear();
+            age = year - yearOfBirth;
+            ages.push(age);
+        } else {
+            ages.push(age);
+        }
+    }
+    let indexOfLargest = 0;
+    let currentLargest = 0;
+    for(let j = 0; j < ages.length; j++) {
+        if(ages[j] > currentLargest) {
+            currentLargest = ages[j];
+            indexOfLargest = j;
+        }
+    }
+    let oldest = arrayOfPeople[indexOfLargest].name;
+    console.log(oldest);
+    return oldest;
 
 };
 
