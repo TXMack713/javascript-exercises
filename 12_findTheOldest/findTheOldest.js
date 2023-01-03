bbb4f9e201485327a77ba34a05baf3672a4266cf
Anthony Mack <mackattack713@gmail.com>
2023-01-03 14:17:10 -0600

    Solved the find the oldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,35 +2,43 @@ const findTheOldest = function(arrayOfPeople) {
     let ages = [];
     for(let i = 0; i < arrayOfPeople.length; i++) {
         let age = 0;
-        if (arrayOfPeople[i].yearOfDeath !== 'undefined') {
-            let yearOfDeath = arrayOfPeople[i].yearOfDeath;
-            let yearOfBirth = arrayOfPeople[i].yearOfBirth;
-            age = yearOfDeath - yearOfBirth;
-            console.log("Age of " + arrayOfPeople[i].name);
-            console.log("Death year: " + yearOfDeath + " minus Birth year: " + yearOfBirth + " equals: " + age);
-            ages.push(age);
-        } else {
-            const date = new Date();
-            const year = date.getFullYear();
+        let yearOfDeath = arrayOfPeople[i].yearOfDeath;
+        let yearOfBirth = arrayOfPeople[i].yearOfBirth;
+        if (!arrayOfPeople[i].hasOwnProperty("yearOfDeath")) {
+            console.log("If block");
+            let date = new Date();
+            let year = date.getFullYear();
             age = year - arrayOfPeople[i].yearOfBirth;
             console.log("For " + arrayOfPeople[i].name + ":");
             console.log("No year of death is available.");
             console.log("The current year is: " + year);
+            console.log("Age of " + arrayOfPeople[i].name);
+            console.log(age);
             console.log("Death year: " + year + " minus Birth year: " + yearOfBirth + " equals: " + age);
             ages.push(age);
+            // continue;
+        } else {
+            console.log("Else block");
+            yearOfDeath = arrayOfPeople[i].yearOfDeath;
+            yearOfBirth = arrayOfPeople[i].yearOfBirth;
+            age = yearOfDeath - yearOfBirth;
+            console.log("Age of " + arrayOfPeople[i].name);
+            console.log("Death year: " + yearOfDeath + " minus Birth year: " + yearOfBirth + " equals: " + age);
+            ages.push(age);
+            // continue;
         }
     }
     let indexOfLargest = 0;
     let currentLargest = 0;
     for(let j = 0; j < arrayOfPeople.length; j++) {
-        if(ages[j] >= currentLargest) {
+        if(ages[j] > currentLargest) {
             currentLargest = ages[j];
             indexOfLargest = j;
         }
     }
     let oldest = arrayOfPeople[indexOfLargest].name;
     console.log("The oldest is: " + oldest);
-    return oldest;
+    return arrayOfPeople[indexOfLargest];
 
 };
 
