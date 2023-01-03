58e1ab7bee9eea0b0a5caeb79df9c06046709c34
Anthony Mack <mackattack713@gmail.com>
2023-01-02 19:11:42 -0600

    Unqualified

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,25 +1,28 @@
 const findTheOldest = function(arrayOfPeople) {
     let ages = [];
     for(let i = 0; i < arrayOfPeople.length; i++) {
-        let yearOfDeath = arrayOfPeople[i].yearOfDeath;
-        let yearOfBirth = arrayOfPeople[i].yearOfBirth;
-        let age = yearOfDeath - yearOfBirth;
-        console.log("Age of " + arrayOfPeople[i].name);
-        console.log(age);
-        if ((arrayOfPeople[i].yearOfDeath == 'null') || (arrayOfPeople[i].yearOfDeath == 'undefined') || (arrayOfPeople[i].yearOfDeath == 'NaN')) {
-            const date = new Date();
-            let year = date.getFullYear();
-            age = year - yearOfBirth;
-            console.log("No year of death is available.");
-            console.log("The current year is: " + year)
+        let age = 0;
+        if (arrayOfPeople[i].yearOfDeath !== 'undefined') {
+            let yearOfDeath = arrayOfPeople[i].yearOfDeath;
+            let yearOfBirth = arrayOfPeople[i].yearOfBirth;
+            age = yearOfDeath - yearOfBirth;
+            console.log("Age of " + arrayOfPeople[i].name);
+            console.log("Death year: " + yearOfDeath + " minus Birth year: " + yearOfBirth + " equals: " + age);
             ages.push(age);
         } else {
+            const date = new Date();
+            const year = date.getFullYear();
+            age = year - arrayOfPeople[i].yearOfBirth;
+            console.log("For " + arrayOfPeople[i].name + ":");
+            console.log("No year of death is available.");
+            console.log("The current year is: " + year);
+            console.log("Death year: " + year + " minus Birth year: " + yearOfBirth + " equals: " + age);
             ages.push(age);
         }
     }
     let indexOfLargest = 0;
     let currentLargest = 0;
-    for(let j = 0; j < ages.length; j++) {
+    for(let j = 0; j < arrayOfPeople.length; j++) {
         if(ages[j] >= currentLargest) {
             currentLargest = ages[j];
             indexOfLargest = j;
@@ -27,7 +30,7 @@ const findTheOldest = function(arrayOfPeople) {
     }
     let oldest = arrayOfPeople[indexOfLargest].name;
     console.log("The oldest is: " + oldest);
-    return arrayOfPeople[indexOfLargest].name;
+    return oldest;
 
 };
 
